0b0b8997c3df72663a92abc3187d73a9c5b862cc
Blossom Perez <blossomperez18@gmail.com>
2020-08-10 10:41:58 -0500

    challenge complete

---
+++ how-much-is-true.js
@@ -0,0 +1,20 @@
+/* Create a function which returns the number of true values there are in an array.
+
+countTrue([true, false, false, true, false]) ➞ 2
+countTrue([false, false, false, false]) ➞ 0
+countTrue([]) ➞ 0
+*/
+let array = [true, false, true, true, false, false, false, false, false];
+let array2 = [];
+function countTrue(arr) {
+    let count = 0;
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] === true) {
+            count++;
+        }
+    }
+    return count;
+}
+
+countTrue(array);
+countTrue(array2);
