94abf640fc16f384ab78ffa9823c51c29ad0c58b
Blossom Perez <blossomperez18@gmail.com>
2020-08-11 09:41:13 -0500

    challenge complete

---
+++ hello-goodbye.js
@@ -0,0 +1,17 @@
+/*Write a function that takes a string name and a number num (either 0 or 1) and return "Hello" + name if num is 1, otherwise return "Bye" + name.
+
+sayHelloBye("alon", 1) ➞ "Hello Alon"
+sayHelloBye("Tomi", 0) ➞ "Bye Tomi"
+sayHelloBye("jose", 0) ➞ "Bye Jose"*/
+function firstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+function sayHelloBye(name, num) {
+    let str = firstLetter(name);
+    if (num === 1) {
+        return `Hello ${str}`
+    } else if (num === 0) {
+        return `Bye ${str}`
+    }
+}
