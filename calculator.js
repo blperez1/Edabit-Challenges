6db18b2d9890882c3d9a507b297a96edd8bbde81
Blossom Perez <blossomperez18@gmail.com>
2020-08-29 23:49:11 -0500

    challenge completed

---
+++ calculator.js
@@ -0,0 +1,19 @@
+// Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
+
+function calculator(num1, operator, num2) {
+    let operators = {
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*": num1 * num2,
+        "/": num1 / num2
+    }
+    return (operator === "/" && num2 === 0) ? "can't divide by 0!" : operators[operator];
+}
+
+console.log(calculator(2, '/', 2))
+console.log(calculator(10, '-', 7))
+console.log(calculator(2, '*', 16))
+console.log(calculator(2, '-', 2))
+console.log(calculator(15, '+', 26))
+console.log(calculator(2, '+', 2))
+console.log(calculator(2, '/', 0))
