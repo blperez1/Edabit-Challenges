ed54ea6ac491e8b5d7b8ea7d704734a0dfd33427
Blossom Perez <blossomperez18@gmail.com>
2020-08-29 21:54:20 -0500

    refactored

--- obj-to-array.js
+++ obj-to-array.js
@@ -4,7 +4,7 @@
 // toArray({ a: 1, b: 2 }) ➞ [["a", 1], ["b", 2]]
 // toArray({ shrimp: 15, tots: 12 }) ➞ [["shrimp", 15], ["tots", 12]]
 // toArray({}) ➞ []
-
+//
 function toArray(obj) {
     return Object.entries(obj)
 }
