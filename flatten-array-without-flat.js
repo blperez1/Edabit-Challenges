fd143adf4205ba5c4c8a891a5a373909a7d0c2b7
Unknown <blossomperez18@gmail.com>
2020-09-06 22:03:18 -0500

    Complete challenge

---
+++ flatten-array-without-flat.js
@@ -0,0 +1,37 @@
+/*I'm trying to write a function to flatten an array of subarrays into one array. (Suppose I am unware there is a .flat() method in the Array prototype). In other words, I want to transform this: [[1, 2], [3, 4]] into [1, 2, 3, 4].
+
+Here is my code:
+
+function flatten(arr) {
+  arr2 = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr2.concat(arr[i]);
+  }
+  return arr2;
+}
+
+But...it doesn't seem to be working! Fix my code so that it correctly flattens the array.
+Examples
+
+flatten([[1, 2], [3, 4]]) ➞ []
+Expected: [1, 2, 3, 4]
+
+flatten([["a", "b"], ["c", "d"]]) ➞ []
+Expected: ["a", "b", "c", "d"] 
+
+flatten([[true, false], [false, false]]) ➞ []
+Expected: [true, false, false, false]*/
+
+// Fix this incorrect code so that all tests pass!
+function flatten(arr) {
+    let arr2 = [];
+    for (let i = 0; i < arr.length; i++) {
+      arr2 = arr2.concat(arr[i]);
+    }
+    return arr2; 
+  }
+
+ const flatten2 = (arr) => arr.reduce((acc, value) => acc.concat(value))
+
+  console.log(flatten([[true, false], [false, false]]))
+  console.log(flatten2([[true, false], [false, false]]))
