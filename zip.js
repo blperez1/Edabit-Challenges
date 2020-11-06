52cc2a85d30ff2f3143c0bc7039ea74cf2431a2a
Unknown <blossomperez18@gmail.com>
2020-11-06 17:02:29 -0600

    Refactor function to use .map() instead of for loop

--- zip.js
+++ zip.js
@@ -1,3 +1,19 @@
+// Given an array of women and an array of men, either:
+
+//     Return "sizes don't match" if the two arrays have different sizes.
+//     If the sizes match, return an array of pairs, with the first woman paired with the first man, second woman paired with the second man, etc.
+
+// Examples
+
+// zipIt(["Elise", "Mary"], ["John", "Rick"])
+//  ➞ [["Elise", "John"], ["Mary", "Rick"]]
+
+// zipIt(["Ana", "Amy", "Lisa"], ["Bob", "Josh"])
+//  ➞ "sizes don't match"
+
+// zipIt(["Ana", "Amy", "Lisa"], ["Bob", "Josh", "Tim"])
+//  ➞ [["Ana", "Bob"], ["Amy", "Josh"],["Lisa", "Tim"]]
+
 function zipIt(women, men) {
     let arr = [];
 	if(women.length !== men.length) {
@@ -9,4 +25,18 @@ function zipIt(women, men) {
     return arr;
 }
 
+function zipIt2(women, men) {
+    if(women.length !== men.length) {
+        return "sizes don't match";
+    }
+
+    return women.map((woman, i) => [woman, men[i]])
+}
+
 console.log(zipIt(["Ana", "Amy", "Lisa"], ["Bob", "Josh", "Tim"]))
+console.log(zipIt(["Elise", "Mary"], ["John", "Rick"]))
+console.log(zipIt(["Ana", "Amy", "Lisa"], ["Bob", "Josh"]))
+
+console.log(zipIt2(["Ana", "Amy", "Lisa"], ["Bob", "Josh", "Tim"]))
+console.log(zipIt2(["Elise", "Mary"], ["John", "Rick"]))
+console.log(zipIt2(["Ana", "Amy", "Lisa"], ["Bob", "Josh"]))
