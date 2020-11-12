d47ddd44644a7d8f6174af18326ca2fd6a8dac17
Unknown <blossomperez18@gmail.com>
2020-11-11 22:20:45 -0600

    Created number-split js file

---
+++ number-split.js
@@ -0,0 +1,10 @@
+// Given a number, return an array containing the two halves of the number. If the number is odd, make the rightmost number higher.
+
+// numberSplit(4) ➞ [2, 2]
+// numberSplit(10) ➞ [5, 5]
+// numberSplit(11) ➞ [5, 6]
+// numberSplit(-9) ➞ [-5, -4]
+
+function numberSplit(n) {
+	
+}
